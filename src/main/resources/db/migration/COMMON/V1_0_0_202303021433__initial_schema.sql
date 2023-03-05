5ebf883a75ddd9a2631366fa788ab7569d2b66a3
Thang Le <tlevan@openwt.com>
2023-03-05 10:44:58 +0700

    Add database

--- src/main/resources/db/migration/COMMON/V1_0_0_202303021433__initial_schema.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_202303021433__initial_schema.sql
@@ -1,7 +1,37 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TABLE roles
+(
+    id    UUID         NOT NULL  PRIMARY KEY  DEFAULT  uuid_generate_v4(),
+    name  VARCHAR(50)  NOT NULL
+);
+
+CREATE TABLE users
+(
+    id         UUID           NOT NULL  PRIMARY KEY  DEFAULT  uuid_generate_v4(),
+    username   VARCHAR(50)    NOT NULL,
+    password   VARCHAR(50)    NOT NULL,
+    firstName  VARCHAR(100),
+    lastName   VARCHAR(100),
+    enabled    BOOLEAN        NOT NULL,
+    avatar     VARCHAR(255),
+    role_id    UUID           NOT NULL,
+    CONSTRAINT fk_roleId
+        FOREIGN KEY (role_id)
+            REFERENCES roles (id)
+);
+
 CREATE TABLE books
 (
-    id   UUID         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(255) NOT NULL
+    id           UUID          NOT NULL  PRIMARY KEY  DEFAULT  uuid_generate_v4(),
+    title        VARCHAR(255)  NOT NULL,
+    author       VARCHAR(255)  NOT NULL,
+    description  VARCHAR(255),
+    created_at   TIMESTAMP     NOT NULL,
+    updated_at   TIMESTAMP     NOT NULL,
+    image        VARCHAR(255),
+    user_id	     UUID          NOT NULL,
+    CONSTRAINT fk_userId
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
 );
--- src/main/resources/db/migration/DEV/V1_0_0_202303021434__add_sample_books.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO books(name)
-VALUES ('The Shawshank Redemption'),
-       ('The Godfather'),
-       ('The Dark Knight '),
-       ('12 Angry Men'),
-       ('The Lord of the Rings: The Return of the King');
