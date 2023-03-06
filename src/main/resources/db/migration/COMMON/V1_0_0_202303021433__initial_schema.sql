fc713fa3a07b34abbb4ea1f7f7032412215384f2
Thang Le <tlevan@openwt.com>
2023-03-06 11:05:06 +0700

    Encode passwords and add fields

--- src/main/resources/db/migration/COMMON/V1_0_0_202303021433__initial_schema.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_202303021433__initial_schema.sql
@@ -10,11 +10,13 @@ CREATE TABLE users
 (
     id         UUID           NOT NULL  PRIMARY KEY  DEFAULT  uuid_generate_v4(),
     username   VARCHAR(50)    NOT NULL,
-    password   VARCHAR(50)    NOT NULL,
+    password   VARCHAR(255)   NOT NULL,
     firstName  VARCHAR(100),
     lastName   VARCHAR(100),
     enabled    BOOLEAN        NOT NULL,
     avatar     VARCHAR(255),
+    created_at TIMESTAMP      NOT NULL,
+    updated_at TIMESTAMP,
     role_id    UUID           NOT NULL,
     CONSTRAINT fk_roleId
         FOREIGN KEY (role_id)
@@ -28,7 +30,7 @@ CREATE TABLE books
     author       VARCHAR(255)  NOT NULL,
     description  VARCHAR(255),
     created_at   TIMESTAMP     NOT NULL,
-    updated_at   TIMESTAMP     NOT NULL,
+    updated_at   TIMESTAMP,
     image        VARCHAR(255),
     user_id	     UUID          NOT NULL,
     CONSTRAINT fk_userId
