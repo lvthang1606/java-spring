1ac77576caa2f5160a109d8b796fd0aa52e7d37b
Thang Le <tlevan@openwt.com>
2023-03-28 23:00:18 +0700

    Update unit-test

--- src/test/java/com/thangle/api/auth/AuthControllerTest.java
+++ src/test/java/com/thangle/api/auth/AuthControllerTest.java
@@ -3,11 +3,11 @@ package com.thangle.api.auth;
 import com.thangle.api.AbstractControllerTest;
 import com.thangle.domain.auth.JWTTokenService;
 import com.thangle.domain.auth.JWTUserDetails;
+import com.thangle.domain.auth.LoginAuthenticationProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
@@ -25,7 +25,7 @@ public class AuthControllerTest extends AbstractControllerTest {
     private static final String BASE_URL = "/api/v1/auths";
 
     @MockBean
-    private AuthenticationManager authenticationManager;
+    private LoginAuthenticationProvider loginAuthenticationProvider;
 
     @MockBean
     private JWTTokenService jwtTokenService;
@@ -35,14 +35,14 @@ public class AuthControllerTest extends AbstractControllerTest {
         final var auth = buildAuthentication();
         final var token = randomAlphabetic(3, 10);
 
-        when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(auth);
+        when(loginAuthenticationProvider.authenticate(any(Authentication.class))).thenReturn(auth);
         when(jwtTokenService.generateToken((JWTUserDetails) auth.getPrincipal())).thenReturn(token);
 
         post(BASE_URL, auth)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.token").value(token));
 
-        verify(authenticationManager).authenticate(any(Authentication.class));
+        verify(loginAuthenticationProvider).authenticate(any(Authentication.class));
         verify(jwtTokenService).generateToken((JWTUserDetails) auth.getPrincipal());
     }
 }
